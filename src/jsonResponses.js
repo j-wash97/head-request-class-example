108362dc1b4e85d42100d36623eef81124ccc227
Julian Washingtion <cybermonkey1997@yahoo.com>
2020-02-06 15:36:48 -0500

    Example Complete

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,31 +2,41 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-
+  response.writeHead(status, {'Content-Type': 'application/json'});
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-
+  response.writeHead(status, {'Content-Type': 'application/json'});
+  response.end();
 };
 
 const getUsers = (request, response) => {
-
+  respondJSON(request, response, 200, { users });
 };
 
 const getUsersMeta = (request, response) => {
-
+  respondJSONMeta(request, response, 200);
 };
 
 const updateUser = (request, response) => {
+  const newUser = { created_at: Date.now() };
 
+  users[newUser.created_at] = newUser;
+
+  respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
-
+  respondJSON(request, response, 404, {
+    message: 'Page was not found',
+    id: 'notFound'
+  });
 };
 
 const notFoundMeta = (request, response) => {
-
+  respondJSONMeta(request, response, 404);
 };
 
 module.exports = {
